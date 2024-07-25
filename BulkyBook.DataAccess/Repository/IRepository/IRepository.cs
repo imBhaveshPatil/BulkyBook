e7d234f1f4646fbdd763dd3152142d1b082d58ee
imBhaveshPatil <bp888121200@gmail.com>
2024-07-25 23:47:03 +0530

    initial commits

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+﻿
+using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;     
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+            return View(categoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category ctgObj)
+        {
+            if (ctgObj.Name == ctgObj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display order cannot exactly match the name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(ctgObj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully.";
+                return RedirectToAction("Index");
+            }
+            return View(ctgObj);
+        }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id is null or 0)
+                return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category ctgObj)
+        {
+            if (ctgObj.Name == ctgObj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display order cannot exactly match the name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(ctgObj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View(ctgObj);
+        }
+        
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if(id is null or 0)
+                return NotFound();
+
+            var category = _db.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var ctgObj = _db.Categories.Find(id);
+            
+            if(ctgObj is null)
+                return NotFound();
+
+            _db.Categories.Remove(ctgObj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
+            return RedirectToAction("Index");
+         
+        }
+    }
+}
